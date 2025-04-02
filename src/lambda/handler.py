252e470636c1ea28f79dee076a635ca9c75ca4b7
Julian Michel <mail@julianmichel.de>
2025-04-02 22:27:40 +0200

    test change

--- src/lambda/handler.py
+++ src/lambda/handler.py
@@ -6,5 +6,5 @@ def lambda_handler(event, context):
     
     return {
         "statusCode": 200,
-        "body": json.dumps("Hello, World!")
+        "body": json.dumps("Hello, World! 2")
     }
--- src/layer/shared.py
+++ src/layer/shared.py
@@ -1,4 +1,4 @@
 def add_numbers(a, b):
     return a + b
 
-# test
+# test 2
